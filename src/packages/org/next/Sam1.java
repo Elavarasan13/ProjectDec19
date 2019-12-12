56a1f6a45dac96996807405dfc3dd7cac830bf4a
Elavarasan13 <elavarasandevaraj@gmail.com>
2019-12-12 10:55:28 +0530

    current process

--- src/packages/org/next/Sam1.java
+++ src/packages/org/next/Sam1.java
@@ -3,7 +3,7 @@ package packages.org.next;
 public class Sam1 {
 	public void id() {
 		System.out.println("id is 123");
-
+		System.out.println("id is 456");
 	}
 	public static void main(String[] args) {
 		Sam1 s=new Sam1();
